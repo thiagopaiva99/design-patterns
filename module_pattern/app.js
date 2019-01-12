35af04bf2f8f36ec43a44228f60bf469576674bb
Thiago Galvani <thiagopaiva99@gmail.com>
2019-01-11 23:26:31 -0200

    Creating a bank account that work with user

--- module_pattern/app.js
+++ module_pattern/app.js
@@ -21,3 +21,42 @@ console.log(person1.getName()); // Thiago Galvani
 
 person2.setName('Thiago', 'Paiva');
 console.log(person2.getName()); // Thiago Paiva
+
+const BankAccount = (function() {
+    let user;
+    let balance = 0;
+
+    return {
+        setUser: (userThatBelongs) => {
+            user = userThatBelongs;
+        },
+        getUser: () => {
+            return `This account belongs to ${user.getName()}`;
+        },
+        getBalance: () => {
+            return `The account have $${balance}`
+        },
+        deposit: (amount) => {
+            balance += amount;
+        },
+        transfer: (amount) => {
+            balance -= amount;
+        }
+    }
+});
+
+const account = new BankAccount();
+
+account.setUser(person1);
+console.log(account.getUser()); // This account belongs to Thiago Galvani
+
+console.log(account.getBalance()); // The account have $0
+account.deposit(20);
+console.log(account.getBalance()); // The account have $20
+account.transfer(10);
+console.log(account.getBalance()); // The account have $10
+
+module.exports = {
+    Person,
+    BankAccount
+};
